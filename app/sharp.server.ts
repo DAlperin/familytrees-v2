fe52b6e4e25cd2a9b62e8a0a11a8a02d32625319
Dov Alperin <dov@fly.io>
2022-12-31 16:24:32 -0500

    Remove pixel limit

--- app/sharp.server.ts
+++ app/sharp.server.ts
@@ -3,7 +3,7 @@ import { rm, rmSync } from "fs";
 
 export async function tileImage(name: string) {
   const version = Date.now();
-  await sharp(`./tmp/uploads/${name}`)
+  await sharp(`./tmp/uploads/${name}`, { limitInputPixels: false })
     .tile({
       layout: "dz",
     })
